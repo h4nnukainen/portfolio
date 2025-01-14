b2a810f7903cc37b10b67b0273d4503b329111f4
Mikko Hannukainen <mikkohannukainen@hotmail.com>
2025-01-14 09:20:25 +0200

    Update job list and small style changes

--- Shared/Job.cs
+++ Shared/Job.cs
@@ -14,6 +14,7 @@ public class Job
     public string Content { get; set; } = "";
     public string Url { get; set; } = "";
     public string Repo { get; set; } = "";
+    public string Time { get; set; } = "";
     public Icons Icon { get; set; } = Icons.Other;
 
 
